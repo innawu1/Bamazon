fd1d23d783dafe56f033410c96a03ba8a661ea72
Ikechi Nnawuba <innawuba.gmail.com>
2017-10-29 14:44:39 -0700

    Updated .js file and README. Added images directory

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Bamazon
+
+##Overview
+-----------
+Bamazon is an innteractive shopping node application that allows users to purchase items as a customer. The app uses MySQL to store inventory and updates as users make purchases.
+
+###Bamazon Customer Portal
+---------------------------
+The Bamazon Customer Portal allows the user to select the ID of the item they wish to purchase and the total number of said item that they wish to purchase. After providing that information, the item ordered, it's price, and the purchase total are displayed for the user.
+
+![alt text](images/bamazon-customer-order.jpg "Bamazon Customer Portal - Order")
+
+The table is then updated to reflect reduction in inventory:
+
+![alt text](images/bamazon-table-update.jpg "Bamazon Customer Portal - Table Update")
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,69 +11,78 @@ var connection = mySql.createConnection({
 });
 
 var purchasedProduct = [];
+
 connection.connect();
 
-connection.query('SELECT ItemID, ProductName, Price FROM Products', function(err, result){
-	if(err){
-		console.log(err);
-	}
+function bamazonCustomerPortal(){
+connection.query('SELECT ItemID, ProductName, Price, StockQuantity FROM Products', function(err, result){
+	if(err) console.log(err);
 
 	var table = new Table({
-		head:['Item ID', 'Product Name', 'Price']
-
+		head:['Item ID', 'Product Name', 'Price', 'Quantity'],
+		colWidths:[10,80,10, 15]
 	});
 
 	for(var i = 0; i < result.length; i++){
-		table.push([result[i].ItemID, result[i].ProductName, result[i].Price]);
+		table.push([result[i].ItemID, result[i].ProductName, result[i].Price, result[i].StockQuantity]);
 	}
 
 	console.log(table.toString());
 	purchase();
-});
+  });
+};
 
-var purchase = function(){
-	var productInfo = {
-		  properties: {
-			itemID: {description: 'Please enter the ID of the item you wish to purchase: '},
-			Qunatity: {description: 'How many would you like to purchase?'}
+	
+function purchase(){
 
-	},
+var productInfo = {
+	properties: {
+			itemID: {description: "Please enter the ID of the item you wish to purchase: "},
+			Quantity: {description: "How many would you like to purchase?"}
+		}
 };
 
 prompt.start();
 
 prompt.get(productInfo, function(err, result){
+
 	var customerPurchase = {
 		itemID: result.itemID,
-		Qunatity: result.Qunatity
+		Quantity: result.Quantity
 	};
 
 	purchasedProduct.push(customerPurchase);
 
 	connection.query('SELECT * FROM Products WHERE ItemID=?', purchasedProduct[0].itemID, function(err, result){
-		if(err){
-			console.log(err, 'Item ID invalid');
-		}
 
-		if(result[0].StockQuantity < purchasedProduct[0].Qunatity){
+		if(err) console.log(err, 'Item ID invalid');
+
+		if(result[0].StockQuantity < purchasedProduct[0].Quantity){
 			console.log('Insufficient quantity!')
 			connection.end();
-		}
 
-		else if(res[0].StockQuantity >= purchasedProduct[0].Qunatity){
-			console.log(purchasedProduct[0].Qunatity + ' items purchased');
+		} else if(result[0].StockQuantity >= purchasedProduct[0].Quantity){
+			console.log(purchasedProduct[0].Quantity + ' items purchased');
 			console.log(result[0].ProductName + ' '+ result[0].Price);
 
-			updatedQuantity = result[0].StockQuantity - purchasedProduct[0].Qunatity;
+			var salesTotal = result[0].Price*purchasedProduct[0].Quantity;
 
-			connection.query("UPDATE Products SET StockQuantity = " + updatedQuantity + "WHERE ItemID= " + purchasedProduct[0].itemID, function(err, result){
-			if(err){
-				console.log(err);
-			}
+			console.log("Totat Cost: " + salesTotal);
+
+			updatedQuantity = result[0].StockQuantity - purchasedProduct[0].Quantity;
+
+			connection.query("UPDATE Products SET StockQuantity = " + updatedQuantity + " WHERE ItemID = " + purchasedProduct[0].itemID, function(err, result){
+			if(err) console.log(err);
+
 			console.log("Your order has been processed. Thank you for shopping at Bamazon");
 			connection.end();
 			})
+			bamazonCustomerPortal();
 		};
-	})
-});
+	 })
+  });
+};
+
+bamazonCustomerPortal();
+
 
